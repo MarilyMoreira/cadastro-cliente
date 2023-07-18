8997dce3c17da1277a77dc0a0dd1eabff4bf5058
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-07-18 15:09:04 -0300

    implementa api de teste

--- src/main/java/br/com/caseirosgourmet/cadastrocliente/CadastroClienteApplication.java
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/CadastroClienteApplication.java
@@ -2,9 +2,19 @@ package br.com.caseirosgourmet.cadastrocliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class CadastroClienteApplication {
+	
+	@GetMapping
+	public String getHomeTeste( ) {
+		return "Cadastro Cliente - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroClienteApplication.class, args);
