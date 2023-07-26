1f06035e17ff96f56ac60bb15547d146d45f317d
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-07-26 16:19:30 -0300

    ajusta mapeamento id uuid

--- src/main/java/br/com/caseirosgourmet/cadastrocliente/domain/Cliente.java
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/domain/Cliente.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,6 +20,8 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Cliente {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
 	private UUID idcliente;
 	@NotBlank
 	private String nomeCompleto;
@@ -31,7 +36,6 @@ public class Cliente {
 	
 	private Cliente(@NotBlank String nomeCompleto, @NotBlank String celular, Endereço endereço,
 			@NotNull LocalDate dataDeNascimento) {
-		this.idcliente = UUID.randomUUID();
 		this.nomeCompleto = nomeCompleto;
 		this.celular = celular;
 		this.endereço = endereço;
