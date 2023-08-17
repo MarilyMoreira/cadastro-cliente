4d0fa150dd702dbbad6169e8173c286d704900f0
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-08-17 17:30:18 -0300

    implementa getTodos

--- src/main/java/br/com/caseirosgourmet/cadastrocliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/application/api/ClienteListResponse.java
@@ -2,15 +2,27 @@ package br.com.caseirosgourmet.cadastrocliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.caseirosgourmet.cadastrocliente.domain.Cliente;
+import lombok.Value;
 
+@Value
 public class ClienteListResponse {
 	private UUID idCliente;
 	private String nomeCompleto;
 	private String celular;
 	
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		return null;
+		return clientes.stream()
+				.map(ClienteListResponse::new)
+				.collect(Collectors.toList());
 	}
+
+	private ClienteListResponse(Cliente cliente) {
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.celular = cliente.getCelular();
+	}
+	
 }
--- src/main/java/br/com/caseirosgourmet/cadastrocliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/application/repository/ClienteRepository.java
@@ -2,7 +2,6 @@ package br.com.caseirosgourmet.cadastrocliente.application.repository;
 
 import java.util.List;
 
-import br.com.caseirosgourmet.cadastrocliente.application.api.ClienteListResponse;
 import br.com.caseirosgourmet.cadastrocliente.domain.Cliente;
 
 public interface ClienteRepository {
