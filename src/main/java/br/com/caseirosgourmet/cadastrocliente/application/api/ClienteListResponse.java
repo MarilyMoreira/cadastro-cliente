5c99d5217d94b3f733f359fc73c1294cbd43593e
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-08-17 16:02:33 -0300

    implementa fluxo repository. teste ok!

--- src/main/java/br/com/caseirosgourmet/cadastrocliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/application/api/ClienteListResponse.java
@@ -1,9 +1,16 @@
 package br.com.caseirosgourmet.cadastrocliente.application.api;
 
+import java.util.List;
 import java.util.UUID;
+
+import br.com.caseirosgourmet.cadastrocliente.domain.Cliente;
 
 public class ClienteListResponse {
 	private UUID idCliente;
 	private String nomeCompleto;
 	private String celular;
+	
+	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
+		return null;
+	}
 }
--- src/main/java/br/com/caseirosgourmet/cadastrocliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/application/repository/ClienteRepository.java
@@ -1,7 +1,11 @@
 package br.com.caseirosgourmet.cadastrocliente.application.repository;
 
+import java.util.List;
+
+import br.com.caseirosgourmet.cadastrocliente.application.api.ClienteListResponse;
 import br.com.caseirosgourmet.cadastrocliente.domain.Cliente;
 
 public interface ClienteRepository {
 	Cliente salva(Cliente cliente);
+	List<Cliente> buscaTodosClientes();
 }
