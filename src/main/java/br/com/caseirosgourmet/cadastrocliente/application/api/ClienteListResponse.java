41f5f2ba36c9e90275f408a687e5d5b5eec606e4
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-08-17 15:28:16 -0300

    implementa fluxo api. teste ok!

---
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/application/api/ClienteListResponse.java
@@ -0,0 +1,9 @@
+package br.com.caseirosgourmet.cadastrocliente.application.api;
+
+import java.util.UUID;
+
+public class ClienteListResponse {
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String celular;
+}
