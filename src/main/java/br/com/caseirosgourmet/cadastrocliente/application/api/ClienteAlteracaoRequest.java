951e580fb722bff3316ccc1821af326529493287
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-09-06 16:41:59 -0300

    implementa fluxo de alteracao camada de API

---
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/application/api/ClienteAlteracaoRequest.java
@@ -0,0 +1,23 @@
+package br.com.caseirosgourmet.cadastrocliente.application.api;
+
+import java.time.LocalDate;
+
+import br.com.caseirosgourmet.cadastrocliente.domain.Endereço;
+import br.com.caseirosgourmet.cadastrocliente.domain.Sexo;
+import jakarta.persistence.Embedded;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class ClienteAlteracaoRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	private String celular;
+	private Sexo sexo;
+	@Embedded
+	private Endereço endereco;	
+	@NotNull
+	private LocalDate dataDeNascimento;
+}
