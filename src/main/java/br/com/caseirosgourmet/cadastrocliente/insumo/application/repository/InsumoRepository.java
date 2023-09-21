ca983e02d6707220155c9caa2e1e9953c7a228de
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-09-21 16:21:30 -0300

    Implementa InsumoRepository

---
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/insumo/application/repository/InsumoRepository.java
@@ -0,0 +1,7 @@
+package br.com.caseirosgourmet.cadastrocliente.insumo.application.repository;
+
+import br.com.caseirosgourmet.cadastrocliente.insumo.domain.Insumo;
+
+public interface InsumoRepository {
+	Insumo salva(Insumo insumo);
+}
--- src/main/java/br/com/caseirosgourmet/cadastrocliente/insumo/application/service/InsumoApplicationService.java
+++ src/main/java/br/com/caseirosgourmet/cadastrocliente/insumo/application/service/InsumoApplicationService.java
@@ -4,18 +4,26 @@ import org.springframework.stereotype.Service;
 
 import br.com.caseirosgourmet.cadastrocliente.insumo.application.api.InsumoRequest;
 import br.com.caseirosgourmet.cadastrocliente.insumo.application.api.InsumoResponse;
+import br.com.caseirosgourmet.cadastrocliente.insumo.application.repository.InsumoRepository;
+import br.com.caseirosgourmet.cadastrocliente.insumo.domain.Insumo;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class InsumoApplicationService implements InsumoService {
+	private final InsumoRepository insumoRepository;
 
 	@Override
 	public InsumoResponse criaInsumo(@Valid InsumoRequest insumoRequest) {
 		log.info("[inicia] InsumoApplicationService - criaInsumo");
+		Insumo insumo = insumoRepository.salva(new Insumo(insumoRequest));
 		log.info("[finaliza] InsumoApplicationService - criaInsumo");
-		return null;
+		return InsumoResponse.builder()
+				.idInsumo(insumo.getIdInsumo())
+				.build();
 	}
 
 }
